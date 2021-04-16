b022a20e55684cde8407aa9b802b06caf1b92f78
agenturallison <edith@agentur-allison.at>
2021-04-16 14:39:07 +0200

    Add hook to email send action. Requested in 24641

--- includes/emails/class-wc-email-customer-completed-order.php
+++ includes/emails/class-wc-email-customer-completed-order.php
@@ -65,9 +65,10 @@ if ( ! class_exists( 'WC_Email_Customer_Completed_Order', false ) ) :
 				$this->placeholders['{order_number}'] = $this->object->get_order_number();
 			}
 
-			if ( $this->is_enabled() && $this->get_recipient() ) {
-				$this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
-			}
+		if ( $this->is_enabled() && $this->get_recipient() ) {
+			$result = (  $this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() ) ) ? TRUE : FALSE;
+			do_action('woocommerce_email_trigger_customer_completed_order', $result, $this);
+		}
 
 			$this->restore_locale();
 		}
